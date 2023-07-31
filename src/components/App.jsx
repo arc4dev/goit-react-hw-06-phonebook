f9f261c536c1d411e66802befa54ffa5b7181b81
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-07-31 19:04:19 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,36 @@
-export const App = () => {
+import { useEffect, useState } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+//App
+function App() {
+  const [filter, setFilter] = useState('');
+  const [isMounted, setIsMounted] = useState(false);
+
+  // useEffect(() => {
+  //   setIsMounted(true);
+  //   getLocalStorage();
+  // }, []);
+
+  // useEffect(() => {
+  //   if (!isMounted) return;
+
+  //   setLocalStorage();
+  // }, [contacts]);
+
+  // const getLocalStorage = () => {
+  //   const contactsArr = JSON.parse(localStorage.getItem('contacts'));
+
+  //   if (!contactsArr) return;
+
+  //   setContacts(contactsArr);
+  // };
+
+  // const setLocalStorage = () => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // };
+
   return (
     <div
       style={{
@@ -7,10 +39,17 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        flexDirection: 'column',
       }}
     >
-      React homework template
+      <h1>PhoneBook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
   );
-};
+}
+
+export default App;
